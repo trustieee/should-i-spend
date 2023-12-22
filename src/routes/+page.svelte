596b7a067166b81de18a9ac1387ae9bd8fcd20bd
Mario <mcacciatore@datadimensions.com>
2023-12-22 00:44:37 -0500

    Add chart

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,101 @@
 <script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
+	import { Chart, registerables } from 'chart.js';
+	Chart.register(...registerables);
 	let amount = '';
-	let years = 30;
-	let increments = 1;
+	let years: number = 30;
+	let increments = 5;
 	let n = 1; // Compounding once per year
 	let rates = [0.03, 0.05, 0.08]; // Interest rates of 3%, 5%, and 8%
 	let tableData: { years: number; rate: number; value: number }[] = [];
+	let canvas: HTMLCanvasElement;
+	let chart: Chart<'line', number[], string>;
+	let colors = ['#FF6347', '#40E0D0', '#ADFF2F']; // Tomato, Turquoise, GreenYellow
 
 	// Function to calculate future value
 	function calculateFutureValue(pv: number, r: number, n: number, t: number) {
-		return pv * Math.pow(1 + r / n, n * t);
+		let fv = pv * Math.pow(1 + r / n, n * t);
+		return fv;
 	}
 
+	$: {
+		// Generate chart labels and datasets
+		let labels = Array.from({ length: years / increments + 1 }, (_, i) => i * increments);
+		let datasets = rates.map((r, index) => {
+			let data = tableData.filter((d) => d.rate === r).map((d) => d.value);
+			return {
+				label: `Interest Rate: ${r * 100}%`,
+				data: data,
+				fill: false,
+				borderColor: colors[index % colors.length] // Use a different color for each line
+			};
+		});
+
+		// Update or create chart
+		if (chart) {
+			chart.data = { labels: labels.map(String), datasets };
+			chart.update();
+		} else if (canvas) {
+			chart = new Chart(canvas.getContext('2d'), {
+				type: 'line',
+				data: { labels, datasets },
+				options: {
+					responsive: true
+					// ... rest of your options ...
+				}
+			});
+		}
+	}
+
+	onMount(() => {
+		// Generate table data
+		for (let t = 0; t <= years; t += increments) {
+			for (let r of rates) {
+				let value = calculateFutureValue(15, r, n, t); // Use 15 as the principal value
+				tableData.push({ years: t, rate: r, value: value });
+			}
+		}
+
+		// Generate chart labels and datasets
+		let labels = Array.from({ length: years / increments + 1 }, (_, i) => i * increments);
+		let datasets = rates.map((r, index) => {
+			let data = tableData.filter((d) => d.rate === r).map((d) => d.value);
+			return {
+				label: `Interest Rate: ${r * 100}%`,
+				data: data,
+				fill: false,
+				borderColor: colors[index % colors.length] // Use a different color for each line
+			};
+		});
+
+		// Create chart
+		chart = new Chart(canvas.getContext('2d'), {
+			type: 'line',
+			data: { labels, datasets },
+			options: {
+				responsive: true,
+				title: {
+					display: true,
+					text: 'Future Value of $1'
+				},
+				scales: {
+					y: {
+						beginAtZero: true,
+						ticks: {
+							// Ensure that ticks are generated for small values
+							precision: 0.01
+						}
+					}
+				}
+			}
+		});
+	});
+
+	onDestroy(() => {
+		if (chart) {
+			chart.destroy();
+		}
+	});
 	function selectOnFocus(node: HTMLInputElement) {
 		node.addEventListener('focus', () => {
 			node.select();
@@ -45,7 +130,9 @@
 		<!-- <h1 class="text-9xl mb-16">${Number(amount).toFixed(2)}</h1> -->
 		<div class="relative flex items-center w-full">
 			<span class="absolute left-0 transform -translate-y-1/2 ml-8 text-6xl">$</span>
+			<!-- svelte-ignore a11y-autofocus -->
 			<input
+				autofocus
 				bind:value={amount}
 				use:selectOnFocus
 				placeholder="Enter an amount"
@@ -92,7 +179,7 @@
 							</tr>
 						</thead>
 						<tbody>
-							{#if amount == 0}
+							{#if +amount === 0}
 								<tr>
 									<td colspan="2" class="text-center italic text-gray-500"
 										>Enter an amount to see the results</td
@@ -111,7 +198,9 @@
 				</div>
 			{/each}
 		</div>
-		<div>chart here</div>
+		<div class="flex justify-center">
+			<canvas bind:this={canvas}></canvas>
+		</div>
 		<p class="text-slate-600 mb-16 mt-16">
 			A = P(1 + [{rates.map((rate) => rate * 100 + '%').join(', ')}])/1)^((1)[0-30])
 		</p>
